2e3dc166305c060b719efcc2391c8e8d9e76af8f
Remigius Mgbeme <garethremigius@gmail.com>
2025-02-06 09:12:14 +0100

    login feature updated to acct_number

--- accounts/views.py
+++ accounts/views.py
@@ -14,7 +14,7 @@ from .models import User
 from .serializers import (
     UserSerializer,
     LoginSerializer,
-    ProfileSerializer,
+    # ProfileSerializer,
 )
 # Create your views here.
 
@@ -57,7 +57,7 @@ class VerifyEmailAddress(APIView):
             user.reset_otp()
             user.save()
             return Response({"Message": "Your email has been verified"}, status=status.HTTP_200_OK)
-        except Account.DoesNotExist:
+        except User.DoesNotExist:
             return Response({"Message": "Invalid OTP"})
 
 
@@ -73,7 +73,6 @@ class LoginUsersView(APIView):
             context = {
                 "name": user.get_full_name(),
                 "last_login": user.last_login,
-                "current_year": timezone.now().year
             }
             html_message = render_to_string('accounts/login_email.html', context)
             send_email(user.email, 'Login Notification', html_message)
@@ -84,16 +83,16 @@ class LoginUsersView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-class ProfileView(APIView):
-    def get(self, request):
-        user = request.user
-        serializer = ProfileSerializer(user)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+# class ProfileView(APIView):
+#     def get(self, request):
+#         user = request.user
+#         serializer = ProfileSerializer(user)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    def put(self, request):
-        user = request.user
-        serializer = ProfileSerializer(user, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request):
+#         user = request.user
+#         serializer = ProfileSerializer(user, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
