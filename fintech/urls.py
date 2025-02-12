bc5d5fb7ca3f10378d3d6d444e79075b896caf79
Remigius Mgbeme <garethremigius@gmail.com>
2025-02-12 07:52:12 +0100

    swagger done

--- fintech/urls.py
+++ fintech/urls.py
@@ -18,8 +18,29 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import permissions  
+from drf_yasg.views import get_schema_view  
+from drf_yasg import openapi  
+from django.urls import path 
+
+
+
+schema_view = get_schema_view(  
+   openapi.Info(  
+      title="Your API Title",  
+      default_version='v1',  
+      description="Test description",  
+      terms_of_service="https://www.yourtermsofservice.com/",  
+      contact=openapi.Contact(email="contact@yourapi.local"),  
+      license=openapi.License(name="BSD License"),  
+   ),  
+   public=True,  
+   permission_classes=(permissions.AllowAny,),  
+)  
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/v1/', include('accounts.urls')),
 ]
