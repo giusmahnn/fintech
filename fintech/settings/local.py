26a0642bec17aa069061d6f061a682d731afb257
giusmahnn <garethremigius@gmail.com>
2025-04-26 00:06:33 +0100

    Package: anymail

--- fintech/settings/local.py
+++ fintech/settings/local.py
@@ -5,3 +5,11 @@ load_dotenv()
 
 DEBUG = True
 BASE_URL = os.getenv('BASE_URL', 'http://127.0.0.1:8000')
+
+
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
+ANYMAIL = {
+    "BREVO_API_KEY": os.getenv("EMAIL_API_KEY"),
+}
